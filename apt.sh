7f6fad0565662ce29fb8803909c3fc08ac3c8636
Takashi Ando <takashi7ando@gmail.com>
2019-05-11 22:35:47 +0900

    Add more tools to install

--- apt.sh
+++ apt.sh
@@ -1,14 +1,23 @@
 #!/usr/bin/env bash
 
-# Assuming you use a Ubuntu you will need the following packages for yocto's build system to work:
-# http://www.yoctoproject.org/docs/2.0/yocto-project-qs/yocto-project-qs.html
-sudo apt install -y \
-    gawk wget git-core diffstat unzip texinfo gcc-multilib \
-    build-essential chrpath socat libsdl1.2-dev xterm
-
 # To build nodejs from yocto, you will need the following packages:
 # https://github.com/imyller/meta-nodejs
 # https://github.com/imyller/meta-nodejs/issues/45
 sudo dpkg --add-architecture i386
 sudo apt update -y
-sudo apt install -y g++-multilib libssl-dev:i386 libcrypto++-dev:i386 zlib1g-dev:i386
+sudo apt install -y \
+    g++-multilib libssl-dev:i386 libcrypto++-dev:i386 zlib1g-dev:i386
+
+# Assuming you use a Ubuntu you will need the following packages for yocto's build system to work:
+# http://www.yoctoproject.org/docs/2.0/yocto-project-qs/yocto-project-qs.html
+sudo apt install -y \
+    gawk wget git-core diffstat unzip texinfo gcc-multilib \
+    build-essential chrpath socat cpio python python3 python3-pip python3-pexpect \
+    xz-utils debianutils iputils-ping libsdl1.2-dev xterm
+
+# for my own dev env
+sudo apt install -y \
+    byobu python-pip python3-pip bpython bpython3 exuberant-ctags
+
+# remove no needed packages
+sudo apt autoremove -y
