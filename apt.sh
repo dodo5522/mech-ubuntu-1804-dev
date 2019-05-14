922e9a482b451b8704c294a1261be71f47b04157
Takashi Ando <takashi7ando@gmail.com>
2019-05-14 11:19:54 +0900

    Add packages to build yocto

--- apt.sh
+++ apt.sh
@@ -6,11 +6,12 @@
 sudo dpkg --add-architecture i386
 sudo apt update -y
 sudo apt install -y \
-    g++-multilib libssl-dev:i386 libcrypto++-dev:i386 zlib1g-dev:i386
+    g++-multilib libssl-dev:i386 libcrypto++-dev:i386 zlib1g-dev:i386 nodejs
 
 # Assuming you use a Ubuntu you will need the following packages for yocto's build system to work:
 # http://www.yoctoproject.org/docs/2.0/yocto-project-qs/yocto-project-qs.html
 sudo apt install -y \
+    libc6-dev-i386 g++-multilib dfu-util \
     gawk wget git-core diffstat unzip texinfo gcc-multilib \
     build-essential chrpath socat cpio python python3 python3-pip python3-pexpect \
     xz-utils debianutils iputils-ping libsdl1.2-dev xterm
